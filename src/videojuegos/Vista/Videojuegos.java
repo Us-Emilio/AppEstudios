b2208b00c294e4ea213eaf29c9e37eed35d7f4d8
esther20682 <moralesesther_93@outlook.es>
2021-01-30 13:46:37 +0100

    Exercici en Java

--- src/videojuegos/Vista/Videojuegos.java
+++ src/videojuegos/Vista/Videojuegos.java
@@ -5,6 +5,13 @@
  */
 package videojuegos.Vista;
 
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import videojuegos.Controlador.LogicaEmpresa;
+import videojuegos.Datos.DriverMySql;
+import videojuegos.Modelo.Empresa;
+
 /**
  *
  * @author Kurin
@@ -14,8 +21,97 @@ public class Videojuegos {
     /**
      * @param args the command line arguments
      */
+    static Scanner in = new Scanner(System.in);
+    static ArrayList<Empresa> empresas = new ArrayList<>();
+
     public static void main(String[] args) {
-        // TODO code application logic here
+
+ 
+        menu();
+
     }
-    
+
+    public static void menu() {
+
+        int opcion = 0;
+        do {
+
+            System.out.println("Sobre que tabla quiere incidir");
+            System.out.println("1. Empresa");
+            System.out.println("2. Videojuego");
+            System.out.println("3. Estudio");
+            System.out.println("0. Salir");
+
+            opcion = pedirEntero("Introduce una opcion del menu");
+
+            switch (opcion) {
+
+                case 1:
+
+                    operacionAefectuar();
+            }
+
+        } while (opcion != 0);
+    }
+
+    public static void operacionAefectuar() {
+
+        int opcion;
+
+        System.out.println("1. Insertar");
+        System.out.println("2. Modificar");
+        System.out.println("3. Eliminar");
+        System.out.println("4. Listar contenido");
+
+        opcion = pedirEntero("¿Qué operacion quieres efectuar?");
+
+        switch (opcion) {
+
+            case 1:
+                Empresa e1 = new Empresa();
+                e1.datosEmpresa();
+                LogicaEmpresa.insertEmpresa(e1);
+                empresas.add(e1);
+
+                break;
+
+            case 2:
+
+                break;
+
+            case 4:
+
+                mostrarEmpresas();
+        }
+
+    }
+
+    public static void mostrarEmpresas() {
+        for (Empresa e : empresas) {
+            if (e != null) {
+                System.out.println(e);
+            }
+        }
+    }
+
+    public static int pedirEntero(String pregunta) {
+        boolean estado = true;
+        int num = 0;
+        String noInt;
+        do {
+
+            try {
+                System.out.println(pregunta);
+                num = in.nextInt();
+                estado = false;
+
+            } catch (InputMismatchException e) {
+                noInt = in.nextLine();
+                System.out.println(noInt + " No es entero!!!");
+
+            }
+        } while (estado);
+        return num;
+    }
+
 }
